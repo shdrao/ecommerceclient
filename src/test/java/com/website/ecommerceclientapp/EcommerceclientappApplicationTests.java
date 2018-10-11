2313e64c212e1d2cbc355174abe6eeb901371b85
shdrao <shdrao@IN-MUM-LDVM038.corp.capgemini.com>
2018-10-11 08:01:36 -0400

    agxdf

---
+++ src/test/java/com/website/ecommerceclientapp/EcommerceclientappApplicationTests.java
@@ -0,0 +1,16 @@
+package com.website.ecommerceclientapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EcommerceclientappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
